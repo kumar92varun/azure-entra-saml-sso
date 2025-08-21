4b4c3caff4dd5fb1d59e2feb22a386df4be12d01
Varun Kumar <kumar92varun@gmail.com>
2025-08-21 17:39:15 +0530

    Added code in reply.php

--- sso/reply.php
+++ sso/reply.php
@@ -1,1 +1,20 @@
-Page where SSO authenticated users are redirected from Azure to the application to verify and process the SAML token.
+<?php
+    require_once '../vendor/autoload.php';
+
+    use OneLogin\Saml2\Auth;
+
+    $settings = require('../ssoConfig.php');
+    $auth = new Auth($settings);
+
+    // Process SAML Response from IdP
+    $auth->processResponse();
+
+    if (!$auth->isAuthenticated()) {
+        echo "Not authenticated!";
+        exit;
+    }
+
+    echo "User is authenticated successfully!<br>Data given to us is:<br>";
+    $userData = $auth->getAttributes();
+    print_r($userData);
+?>
